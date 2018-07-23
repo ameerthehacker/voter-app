7336074d66d69c8e774e52c366113dee7520049f
ameerthehacker <ameerjhanprof@gmail.com>
2018-07-23 21:13:12 +0530

    Add the integration tests

--- src/app/components/voting/voting.component.spec.ts
+++ src/app/components/voting/voting.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from "@angular/platform-browser";
 
 import { VotingComponent } from './voting.component';
 
@@ -21,5 +22,32 @@ describe('VotingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should increase the vote when upvoted', () => {
+    // Arrange
+    fixture = TestBed.createComponent(VotingComponent);
+    component = fixture.componentInstance;
+    const button = fixture.debugElement.query(By.css('#up-vote'));
+    // Act
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    // Assert
+    const votes = fixture.debugElement.query(By.css('.votes'));
+    expect(votes.nativeElement.textContent).toBe(component.votes.toString());
+  });
+
+  it("should decrease the vote when upvoted", () => {
+    // Arrange
+    fixture = TestBed.createComponent(VotingComponent);
+    component = fixture.componentInstance;
+    const button = fixture.debugElement.query(By.css("#down-vote"));
+    component.votes = 2;
+    // Act
+    button.triggerEventHandler("click", null);
+    fixture.detectChanges();
+    // Assert
+    const votes = fixture.debugElement.query(By.css(".votes"));
+    expect(votes.nativeElement.textContent).toBe(component.votes.toString());
   });
 });
